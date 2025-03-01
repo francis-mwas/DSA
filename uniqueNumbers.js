5694023d9cf94cad33864f445d8e660d38723bd1
francis-mwas <mwangif184@gmail.com>
2025-03-01 23:37:44 +0300

    unique numbers using multiple counter pattern

--- uniqueNumbers.js
+++ uniqueNumbers.js
@@ -1,3 +1,4 @@
+//Method 1: using frequency counter pattern
 function countUniqueNumbers(arr) {
   const unique = [];
   let hashMap = {};
@@ -13,6 +14,23 @@ function countUniqueNumbers(arr) {
   }
   return unique.length;
 }
+
+//Method 2: using multiple pointers pattern
+function countUniqueNumbers2(arr) {
+  if (arr.length === 0) return 0;
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+
+console.log(countUniqueNumbers2([1, 2, 2, 3, 4, 4, 5, 6, 7, 8]));
+console.log(countUniqueNumbers2([]));
+
 console.log(countUniqueNumbers([1, 2, 2, 3, 4, 4, 5, 6, 7, 8]));
 console.log(countUniqueNumbers([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
 console.log(countUniqueNumbers([10, 20, 20, 30, 30, 40, 40, 50]));
